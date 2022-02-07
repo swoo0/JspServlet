6408ce5b395da38ab108cc9d1d81b0275827f4b6
Woo <swoo0@naver.com>
2022-02-07 13:51:59 +0900

    결합도 낮춤

--- JSP_datasource/src/com/jsp/controller/MemberListServlet.java
+++ JSP_datasource/src/com/jsp/controller/MemberListServlet.java
@@ -1,27 +1,74 @@
 package com.jsp.controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.jsp.dao.MemberDAO;
+import com.jsp.dao.MemberDAOImpl;
+import com.jsp.datasource.OracleMybatisSqlSessionFactory;
+import com.jsp.dto.MemberVO;
+import com.jsp.service.MemberService;
+import com.jsp.service.MemberServiceImpl;
+
 public class MemberListServlet extends HttpServlet {
 
+	private MemberService memberService;
 	
 	
 	@Override
-	public void init() throws ServletException {
-		// TODO Auto-generated method stub
-		super.init();
+	public void init(ServletConfig config) throws ServletException {
+//		SqlSessionFactory sqlSessionFactory = new OracleMybatisSqlSessionFactory();
+//		MemberDAO memberDAO = new MemberDAOImpl();
+//		memberService = new MemberServiceImpl();
+		String memberServiceStr = config.getInitParameter("memberService");
+		String memberDAOStr = config.getInitParameter("memberDAO");
+		String sqlSessionFactoryStr = config.getInitParameter("sqlSessionFactory");
+		
+		try {
+			memberService = (MemberService) Class.forName(memberServiceStr).newInstance();
+			MemberDAO memberDAO = (MemberDAO) Class.forName(memberDAOStr).newInstance();
+			SqlSessionFactory sqlSessionFactory = (SqlSessionFactory) Class.forName(sqlSessionFactoryStr).newInstance();
+		} catch (Exception e) {
+			
+		}
+		
+		
+		if (memberService instanceof MemberServiceImpl) {
+			MemberServiceImpl memberServiceImpl = (MemberServiceImpl) memberService;
+			memberServiceImpl.setSqlSessionFactory(sqlSessionFactory);
+//			((MemberServiceImpl) memberService).setSqlSessionFactory(sqlSessionFactory);
+			memberServiceImpl.setMemberDAO(memberDAO);
+			
+			System.out.println("memberService injection clear!");
+		}
 	}
+	
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		String url = "/WEB-INF/views/member/list.jsp";
+		
+		List<MemberVO> memberList = null;
+		
+		try {
+			memberList = memberService.getMemberList();
+		} catch (SQLException e) {
+			url = "/WEB-INF/views/member/list_fail.jsp";
+			e.printStackTrace();
+		}
+		
+		req.setAttribute("memberList", memberList);
+		
+		req.getRequestDispatcher(url).forward(req, resp);
 	}
 	
 	@Override
